7e465d509cbebd8c140b29eb0783b68784e98897
Vaishnav Bharadwaj <vaishnav@Vaishnavs-MacBook-Air.local>
2023-10-13 22:25:01 +0100

    card added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import {ThemeProvider } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles'
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,12 +8,25 @@ import Search from './components/Search/Search';
 import CurrentWeather from './components/current-weather/CurrentWeather';
 
 function App() {
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        'Poppins', 
+        "sans-serif"
+      ].join(','),
+    }
+  })
   return (
-    <div className="main_card">
-      <h1>Weather</h1>
-      <Search />
-      <CurrentWeather/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="main_card">
+        <Typography variant="h3" style={{ fontFamily: 'Poppins' }}>
+          Weather
+        </Typography>
+        <Search />
+        <CurrentWeather/>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -1,9 +1,35 @@
 import React from 'react'
+// import "./CurrentWeather.css"
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const CurrentWeather = () => {
-  return (
-    <div>CurrentWeather</div>
-  )
+    return (
+        <Card sx={{ maxWidth: "500px", color:"#fff",marginTop: '20px', backgroundColor: "#222222"}}>
+            <CardContent>
+                <Typography sx={{textAlign: "center"}} variant="h4" component="div">
+                Bangalore
+                </Typography>
+            </CardContent>
+        
+            <CardMedia
+                sx={{ height: "110px" , width:"80px", margin:"auto"}}
+                image="/icons/01d.png"
+                title="Sunny"
+            />
+
+            <CardContent>
+                <Typography sx={{textAlign: "center"}} variant="h4" component="div">
+                27°c
+                </Typography>
+            </CardContent>
+          
+        </Card>
+      );
 }
 
 export default CurrentWeather
